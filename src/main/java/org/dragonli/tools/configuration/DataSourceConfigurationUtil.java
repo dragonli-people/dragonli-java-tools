c03efb2a9865b2c0bb4d227849680426bca0e693
315920958@qq.com <315920958@qq.com>
2019-10-21 15:50:05 +0800

    去除异常

--- src/main/java/org/dragonli/tools/configuration/DataSourceConfigurationUtil.java
+++ src/main/java/org/dragonli/tools/configuration/DataSourceConfigurationUtil.java
@@ -23,12 +23,11 @@ public class DataSourceConfigurationUtil extends ConfigurationUtilBase {
      *               the path of type is ${prefix}.type,the path of dataSourceConfigPath is ${prefix}.data-config.
      *               "type" and "data-config" can be defined in service.general.db-resource-type-suffix and service.general.db-resource-config-suffix
      * @return a DataSource instance
-     * @throws Exception general Exception
      */
-    public DataSource getDataSource(String prefix) throws Exception{
-        String type = configurableEnvironment.getProperty( prefix + "." + typeSuffix , "com.zaxxer.hikari.HikariDataSource" ) ;
-        String key = prefix + "." + configSuffix;
-        return getDataSource(type,key);
+    public DataSource getDataSource(String prefix) {
+            String type = configurableEnvironment.getProperty( prefix + "." + typeSuffix , "com.zaxxer.hikari.HikariDataSource" ) ;
+            String key = prefix + "." + configSuffix;
+            return getDataSource(type,key);
     }
 
     /**
@@ -36,14 +35,17 @@ public class DataSourceConfigurationUtil extends ConfigurationUtilBase {
      * @param type DataSource type : one of in org.springframework.boot.jdbc.DataSourceBuilder.DATA_SOURCE_TYPE_NAMES
      * @param dataSourceConfigPath the config path of datasource
      * @return a DataSource instance
-     * @throws Exception general Exception
      */
-    public DataSource getDataSource(String type,String dataSourceConfigPath) throws Exception{
-        if(type == null || "".equals(type=type.trim()))type = "com.zaxxer.hikari.HikariDataSource";
-        Class<? extends DataSource> clazz = (Class<? extends DataSource>)Class.forName(type);
-        DataSource dataSource = DataSourceBuilder.create().type(clazz).build();
-        BindResult<DataSource> result = binder.bind(dataSourceConfigPath, Bindable.ofInstance(dataSource));
-        return result.get();
+    public DataSource getDataSource(String type,String dataSourceConfigPath){
+        try {
+            if(type == null || "".equals(type=type.trim()))type = "com.zaxxer.hikari.HikariDataSource";
+            Class<? extends DataSource> clazz = (Class<? extends DataSource>)Class.forName(type);
+            DataSource dataSource = DataSourceBuilder.create().type(clazz).build();
+            BindResult<DataSource> result = binder.bind(dataSourceConfigPath, Bindable.ofInstance(dataSource));
+            return result.get();
+        }catch (Exception e){}
+
+        return null;
     }
 
 
